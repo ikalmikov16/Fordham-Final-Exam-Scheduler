06307bdbe072f201c6bf17a49c8afac329d8bc04
ikalmikov16 <ikalmikov@gmail.com>
2024-10-22 13:35:18 -0400

    Added migrations

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,13 +7,13 @@ class Course(models.Model):
 
     major_tag = models.CharField(max_length=4)
     number = models.CharField(max_length=4)
+    major_and_number = models.CharField(max_length=9)
     section = models.CharField(max_length=3)
     crn = models.CharField(max_length=5, unique=True)
 
-    location = models.CharField(max_length=10)
-    exam_start_time = models.TimeField()
-    exam_end_time = models.TimeField()
-    exam_date = models.DateField()
+    location = models.CharField(max_length=20)
+    exam_start_time = models.DateTimeField()
+    exam_end_time = models.DateTimeField()
 
     def __str__(self):
         return f"{self.major_tag} {self.number} - {self.section} - {self.crn}"
