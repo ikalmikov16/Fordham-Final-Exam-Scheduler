7d040c18e0c8999bfa053ed1039080a975f19362
ikalmikov16 <ikalmikov@gmail.com>
2024-12-02 03:08:48 -0500

    Removed google analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,13 @@ import HomePage from "./pages/HomePage";
 import ReactGA from "react-ga4";
 import { useEffect } from "react";
 
-ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_ID);
+// ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_ID);
 
 function App() {
   return (
     <>
       <Router>
-        <PageTracker />
+        {/* <PageTracker /> */}
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -25,14 +25,14 @@ function App() {
   );
 }
 
-function PageTracker() {
-  const location = useLocation();
+// function PageTracker() {
+//   const location = useLocation();
 
-  useEffect(() => {
-    ReactGA.send({ hitType: "pageview", page: location.pathname });
-  }, [location]);
+//   useEffect(() => {
+//     ReactGA.send({ hitType: "pageview", page: location.pathname });
+//   }, [location]);
 
-  return null;
-}
+//   return null;
+// }
 
 export default App;
